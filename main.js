f4d4e69d41c5c49f8d7d2be5fcb87c6571f4b7da
RaviNarasimha41 <ravinarasimha41@gmail.com>
2023-05-12 12:44:48 +0530

    iniy

---
+++ main.js
@@ -0,0 +1,7 @@
+let bar = document.querySelector('.bars'),
+navItem = document.querySelector('.nav-items');
+
+bar.addEventListener('click', () => {
+    navItem.classList.toggle('active');
+})
+Footer
