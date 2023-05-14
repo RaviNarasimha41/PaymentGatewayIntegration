74b961c63ef6bb3cc65e06070bc529b03e1304fe
RAVI NARASIMHA <118276282+RaviNarasimha41@users.noreply.github.com>
2023-05-14 23:52:56 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-let bar = document.querySelector('.bars'),
-navItem = document.querySelector('.nav-items');
-
-bar.addEventListener('click', () => {
-    navItem.classList.toggle('active');
-})
-Footer
